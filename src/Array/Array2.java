79639e0438a764275a6d3d535482c295f91908d1
SNEHA RANI <sneharani879@gmail.com>
2026-01-07 11:11:05 +0530

    Delete src/Array directory

--- src/Array/Array2.java
+++
@@ -1,9 +0,0 @@
-package Array;
-public class Array2 {
-    public static void main(String[]args){
-        int arr[]={1,5,23,2};//------Declaration,Instalization,Initialization
-        for(int i=0;i<arr.length;i++){//-------traversing array
-            System.out.println(arr[i]);
-        }
-    }
-}
