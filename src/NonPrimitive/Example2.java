ecb25e99e37693cd073b6b1cb91ec3fea81ce941
SNEHA RANI <sneharani879@gmail.com>
2026-01-07 11:12:00 +0530

    Delete src/NonPrimitive directory

--- src/NonPrimitive/Example2.java
+++
@@ -1,9 +0,0 @@
-package NonPrimitive;
-public class Example2 {
-    public static void main(String[]args){
-        String a="Java ia a powerful language";
-        System.out.println(a);
-        String b=a.replace(" ", "_");
-        System.out.println(b);
-    }
-}
--- src/NonPrimitive/Example5.java
+++
@@ -1,7 +0,0 @@
-package NonPrimitive;
-public class Example5 {
-    public static void main(String[]args){
-        String letter="Dear Harsh,\nI hope you are doing well.\nThanks & Regards";
-        System.out.println(letter);
-    }
-}
