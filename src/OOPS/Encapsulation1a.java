fcff84293c10ac5e61f8bf19c9aa606e93041377
SNEHA RANI <sneharani879@gmail.com>
2026-01-07 11:12:12 +0530

    Delete src/OOPS directory

--- src/OOPS/Encapsulation1a.java
+++
@@ -1,9 +0,0 @@
-package OOPS;
-class Encapsulation1a {
-    public static void main(String[]args){
-        Encapsulation1 e=new Encapsulation1();
-        e.setName("Harsh");
-        System.out.println(e.getName());
-    }
-
-}
